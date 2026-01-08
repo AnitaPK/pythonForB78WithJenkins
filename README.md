6e54aeb8b9c4f37aa340cd68a7c553533ac78515
AnitaPK <anita.khamitkar@gmail.com>
2026-01-08 15:42:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/AnitaPK/pythonForB78WithJenkins.git
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # some sample data
+    users = [
+        {"name": "Rahul", "age": 25, "email": "rahul@gmail.com"},
+        {"name": "Anita", "age": 28, "email": "anita@gmail.com"},
+        {"name": "Amit", "age": 30, "email": "amit@gmail.com"}
+    ]
+    return render_template("index.html", users=users)
+
+if __name__ == "__main__":
+    app.run(debug=True)
